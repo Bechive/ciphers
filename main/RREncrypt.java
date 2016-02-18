58ea97c526dc5c965c6400d4806b59f4728acecc
0x42656e <0x42656e>
2016-02-19 05:09:59 +1100

    works for multiple lines

--- main/RREncrypt.java
+++ main/RREncrypt.java
@@ -3,33 +3,34 @@ public class RREncrypt
   public static String RREncrypt(String inputCipher, int linesNo)
   {
       int i;
+      int q; //for the middle lines
       int p = inputCipher.length();
       String[] lines;
       lines = new String[linesNo];
 
-      lines[0] = "";
-      lines[1] = " ";
-      lines[2] = " ";
-
       //For the first line
+      lines[0] = "";
       for (i=0; i<p; i+= (linesNo-1)*2)
       {
         lines[0] += inputCipher.charAt(i);
       }
 
-      //For the second line
-      for (i=1; i<p; i+=(2))
+      //For multiple lines
+      for (q=0; q<linesNo-1; q++)
       {
-        lines[1] += inputCipher.charAt(i);
+      lines[q] = " ";
+        for (i=q; i<p; i+=2)
+        {
+          lines[q] += inputCipher.charAt(i);
+        }
       }
 
       //For the last line
+      lines[linesNo-1] = " ";
       for (i=linesNo-1; i<p; i+= (linesNo-1)*2)
       {
         lines[2] += inputCipher.charAt(i);
       }
-
-
 
       String outputCipher = "";
       for (i=0; i<linesNo; i++)
