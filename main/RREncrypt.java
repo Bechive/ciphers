a1277248bfb3605194fcd26ac4a371ba782e7066
0x42656e <0x42656e>
2016-02-19 04:51:10 +1100

    first line of encrypted text works

--- main/RREncrypt.java
+++ main/RREncrypt.java
@@ -2,15 +2,33 @@ public class RREncrypt
 {
   public static String RREncrypt(String inputCipher, int linesNo)
   {
+      int i;
+      int p = inputCipher.length();
+      String[] lines;
+      lines = new String[linesNo];
+
+      lines[0] = "";
+      lines[1] = "";
+      lines[2] = "";
+
+      //For the first line
+      for (i=0; i<p; i+= (linesNo-1)*2)
+      {
+        lines[0] += inputCipher.charAt(i);
+      }
 
 
 
 
 
 
-      String[] lines = {"ss", "qq", "dd"};
 
-      String outputCipher = lines[0] + lines[1] + lines[2];
+
+      String outputCipher = "";
+      for (i=0; i<linesNo; i++)
+      {
+        outputCipher += lines[i];
+      }
       return outputCipher;
   }
 }
