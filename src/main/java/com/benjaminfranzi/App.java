73796ad2ede555045b09c610ae67685648d3acd7
Benjamin Franzi <benjamin.franzi@outlook.com>
2016-07-25 23:40:28 +1000

    fxml working

--- src/main/java/com/benjaminfranzi/App.java
+++ src/main/java/com/benjaminfranzi/App.java
@@ -1,9 +1,25 @@
 package com.benjaminfranzi;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Ciphers" );
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
     }
 }
--- src/main/java/com/benjaminfranzi/user/controller/MainController.java
+++ src/main/java/com/benjaminfranzi/user/controller/MainController.java
@@ -1,7 +1,5 @@
 package com.benjaminfranzi.user.controller;
 
-/**
- * Created by epic on 25/07/16.
- */
 public class MainController {
+
 }
